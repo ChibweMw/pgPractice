e8f61abfe7f198678122e34f8a2476c8afff5185
Chibwe Mweene <caleidosymmetry@gmail.com>
2018-05-30 08:08:38 +0000

    knex connected

---
+++ search_people.js
@@ -0,0 +1,44 @@
+// var tableSearch = process.argv[2];
+const settings = require("./settings");
+const knex = require("knex")({
+  client: 'pg',
+  version: '7.4',
+  connection: {
+    host : settings.hostname,
+    user : settings.user,
+    password : settings.password,
+    database : settings.database
+  }
+});
+
+knex.select('first_name').from('famous_people')
+  .asCallback((err, res) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(res)
+    }
+    knex.destroy()
+  })
+// let myQuery = function () {
+//   client.query("SELECT * FROM famous_people WHERE first_name = $1::VARCHAR(50)", [tableSearch], (err, result) => {
+//     if (err) {
+//       return console.error("error running query", err);
+//     } else {
+//       console.log("Searching...");
+//     }
+//     let rowCount = 1;
+//     console.log(`Found ${result.rows.length} person(s) by the name '${tableSearch}'`);
+//     result.rows.forEach(function (person) {
+//       console.log(`- ${rowCount++}: ${person.first_name} ${person.last_name}, born '${person.birthdate.getFullYear()}-0${person.birthdate.getMonth() + 1}-${person.birthdate.getDate()}'`)
+//     })
+//     client.end();
+//   });
+// }
+
+// client.connect((err) => {
+//   if(err) {
+//     return console.error("Connection Error", err);
+//   }
+//   myQuery();
+// });
