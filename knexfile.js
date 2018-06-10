396909d5bc57e5304e841dcdaa50573630ba7e61
Chibwe Mweene <caleidosymmetry@gmail.com>
2018-06-10 21:55:00 +0000

    migration for 'milestones' table created

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+// Update with your config settings.
+const settings = require("./settings");
+
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: settings.database,
+      user:     settings.user,
+      password: settings.password
+    }
+  },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
